6dc21deb69c47e3ac8c02e79b1aac55933ad6fe4
Nagendra Rao Jogi <rao.jogi@gmail.com>
2020-10-08 13:10:48 -0400

    added menu item >> feedback at the bottom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [CommonModule, BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
